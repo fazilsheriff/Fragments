e236285cc81aa2889f8fe26b3388aacd4241d911
fazil.s@lantrasoft.com <SinCos123>
2020-10-12 09:04:41 +0530

    Simple Fragment

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Create Instance of Fragment
+        val firstFragment=FirstFragment()
+        //Creating  a fragment manager
+        val manager=supportFragmentManager
+        // Creating a transaction
+        val transacion=manager.beginTransaction()
+        // Adding fragment dynamically
+        transacion.add(R.id.fragmentLayout,firstFragment)
+        transacion.commit()
+
+
     }
 }
